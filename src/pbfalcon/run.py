c12a1cfb4c0f8bb221ef33690f5542c8418f47a8
Christopher Dunn <cdunn@pacificbiosciences.com>
2015-08-10 14:30:11 -0700

    falcon no-op, but specify config-file

--- src/pbfalcon/run.py
+++ src/pbfalcon/run.py
@@ -1,4 +1,6 @@
+from falcon_kit import run_support as support
 import pbcommand.cli
+import pbsmrtpipe.cli_utils
 import pbsmrtpipe.utils
 
 import logging
@@ -7,18 +9,26 @@ import sys
 __version__ = '1.0.0'
 log = logging.getLogger()
 
-def _run_from_args(args):
-    print("_run_from_args(%s)" %repr(args))
-    return 0
-
+def _add_an_option(p):
+    p.add_argument('config',
+        type=pbsmrtpipe.cli_utils.validate_file,
+        help="Specify path to config file.")
+    return p
 def _get_parser():
     desc = "Runner for FALCON with pbsmrtpipe jobs."
     p = pbcommand.cli.get_default_argparser(__version__, desc)
     funcs = [
+        _add_an_option
     ]
-    #f = pbsmrtpipe.utils.compose(*funcs)
-    #p = f(p)
+    f = pbsmrtpipe.utils.compose(*funcs)
+    p = f(p)
     return p
+
+def _run_from_args(args):
+    print("_run_from_args(%s)" %repr(args))
+    log.info( "pb-falcon started with configuration %s", args.config ) 
+    config = support.get_config(support.parse_config(args.config))
+    return 0
 
 def main(argv=sys.argv):
     print(argv)
