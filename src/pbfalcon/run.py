56ac63218c2e7ac5e1c76a1be8e15e2c73c8a5a5
Christopher Dunn <cdunn@pacificbiosciences.com>
2015-08-10 07:35:27 -0700

    no compose(funcs), but passing

--- src/pbfalcon/run.py
+++ src/pbfalcon/run.py
@@ -1,4 +1,28 @@
+import pbcommand.cli
+import pbsmrtpipe.utils
+
+import logging
 import sys
+
+__version__ = '1.0.0'
+log = logging.getLogger()
+
+def _run_from_args(args):
+    print("_run_from_args(%s)" %repr(args))
+    return 0
+
+def _get_parser():
+    desc = "Runner for FALCON with pbsmrtpipe jobs."
+    p = pbcommand.cli.get_default_argparser(__version__, desc)
+    funcs = [
+    ]
+    #f = pbsmrtpipe.utils.compose(*funcs)
+    #p = f(p)
+    return p
 
 def main(argv=sys.argv):
     print(argv)
+    parser = _get_parser()
+    return pbcommand.cli.pacbio_args_runner(
+            argv[1:], parser, _run_from_args,
+            log, pbsmrtpipe.utils.setup_log)
