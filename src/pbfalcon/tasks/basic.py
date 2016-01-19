11b513b1d929f06208cbc8dc67e59777277ed360
Christopher Dunn <cdunn@pacificbiosciences.com>
2016-01-19 11:19:53 -0800

    FT_*

--- src/pbfalcon/tasks/basic.py
+++ src/pbfalcon/tasks/basic.py
@@ -18,12 +18,14 @@ DRIVER_BASE = "python -m pbfalcon.tasks.basic "
 #registry = pbquick.registry_builder(TOOL_NAMESPACE, DRIVER_BASE)
 registry = registry_builder(TOOL_NAMESPACE, DRIVER_BASE)
 
-# FOFN = FileType(to_file_ns("generic_fofn"), "generic", "fofn", 'text/plain')
-FC_FOFN = FileTypes.FOFN
-FC_JSON = FileTypes.JSON
-FC_CONFIG = FileTypes.TXT
-FC_BASH = FileTypes.TXT
-FC_DUMMY = FileTypes.TXT
+# FT_FOFN = FileType(to_file_ns("generic_fofn"), "generic", "fofn", 'text/plain')
+FT_FOFN = FileTypes.FOFN
+FT_JSON = FileTypes.JSON
+FT_CFG = FileTypes.TXT
+FT_BASH = FileTypes.TXT
+FT_DUMMY = FileTypes.TXT
+FT_SUBREADS = FileTypes.DS_SUBREADS
+FT_FASTA = FileTypes.FASTA
 
 def FT(file_type, basename):
     return OutputFileType(file_type.file_type_id,
@@ -31,30 +33,30 @@ def FT(file_type, basename):
                           repr(file_type),
                           "description for {f}".format(f=file_type),
                           basename)
-RDJ = FT(FC_BASH, 'run_daligner_jobs.sh')
+RDJ = FT(FT_BASH, 'run_daligner_jobs.sh')
 
 
-@registry('task_falcon_config_get_fasta', '0.0.0', [FC_CONFIG], [FC_FOFN], is_distributed=False)
+@registry('task_falcon_config_get_fasta', '0.0.0', [FT_CFG], [FT_FOFN], is_distributed=False)
 def run_rtc(rtc):
   with cd(os.path.dirname(rtc.task.output_files[0])):
     return pbfalcon.run_falcon_config_get_fasta(rtc.task.input_files, rtc.task.output_files)
 
-@registry('task_falcon_config', '0.0.0', [FC_CONFIG, FC_FOFN], [FC_JSON], is_distributed=False)
+@registry('task_falcon_config', '0.0.0', [FT_CFG, FT_FOFN], [FT_JSON], is_distributed=False)
 def run_rtc(rtc):
   with cd(os.path.dirname(rtc.task.output_files[0])):
     return pbfalcon.run_falcon_config(rtc.task.input_files, rtc.task.output_files)
 
-@registry('task_falcon_make_fofn_abs', '0.0.0', [FC_FOFN], [FC_FOFN], is_distributed=False)
+@registry('task_falcon_make_fofn_abs', '0.0.0', [FT_FOFN], [FT_FOFN], is_distributed=False)
 def run_rtc(rtc):
   with cd(os.path.dirname(rtc.task.output_files[0])):
     return pbfalcon.run_falcon_make_fofn_abs(rtc.task.input_files, rtc.task.output_files)
 
-@registry('task_falcon0_build_rdb', '0.0.0', [FC_JSON, FC_FOFN], [RDJ, FT(FC_DUMMY, 'job.done')], is_distributed=False)
+@registry('task_falcon0_build_rdb', '0.0.0', [FT_JSON, FT_FOFN], [RDJ, FT(FT_DUMMY, 'job.done')], is_distributed=False)
 def run_rtc(rtc):
   with cd(os.path.dirname(rtc.task.output_files[0])):
     return pbfalcon.run_falcon_build_rdb(rtc.task.input_files, rtc.task.output_files)
 
-@registry('task_falcon0_run_daligner_jobs', '0.0.0', [FC_JSON, RDJ], [FC_FOFN], is_distributed=True, nproc=4)
+@registry('task_falcon0_run_daligner_jobs', '0.0.0', [FT_JSON, RDJ], [FT_FOFN], is_distributed=True, nproc=4)
 def run_rtc(rtc):
   with cd(os.path.dirname(rtc.task.output_files[0])):
     return pbfalcon.run_daligner_jobs(rtc.task.input_files, rtc.task.output_files, db_prefix='raw_reads')
@@ -68,35 +70,35 @@ def run_rtc(rtc):
 # Because this is I/O bound, we do not really harm the machine we are on,
 # but we need to reserve some memory. nproc=6 is more than enough.
 # TODO: Move into /tmp, to reduce the burden on NFS. Then we might chunk.
-@registry('task_falcon0_run_merge_consensus_jobs', '0.0.0', [FC_JSON, RDJ, FC_FOFN], [FC_FOFN], is_distributed=True, nproc=6)
+@registry('task_falcon0_run_merge_consensus_jobs', '0.0.0', [FT_JSON, RDJ, FT_FOFN], [FT_FOFN], is_distributed=True, nproc=6)
 def run_rtc(rtc):
   with cd(os.path.dirname(rtc.task.output_files[0])):
     return pbfalcon.run_merge_consensus_jobs(rtc.task.input_files, rtc.task.output_files, db_prefix='raw_reads')
 
 # Run similar steps for preads.
-@registry('task_falcon1_build_pdb', '0.0.0', [FC_JSON, FC_FOFN], [RDJ], is_distributed=False)
+@registry('task_falcon1_build_pdb', '0.0.0', [FT_JSON, FT_FOFN], [RDJ], is_distributed=False)
 def run_rtc(rtc):
   with cd(os.path.dirname(rtc.task.output_files[0])):
     return pbfalcon.run_falcon_build_pdb(rtc.task.input_files, rtc.task.output_files)
 
-@registry('task_falcon1_run_daligner_jobs', '0.0.0', [FC_JSON, RDJ], [FC_FOFN], is_distributed=True, nproc=4)
+@registry('task_falcon1_run_daligner_jobs', '0.0.0', [FT_JSON, RDJ], [FT_FOFN], is_distributed=True, nproc=4)
 def run_rtc(rtc):
   with cd(os.path.dirname(rtc.task.output_files[0])):
     return pbfalcon.run_daligner_jobs(rtc.task.input_files, rtc.task.output_files, db_prefix='preads')
 
 # Actually, this skips consensus.
 # TODO: Split this into 2 in pipelines, maybe. (But consider running in /tmp.)
-@registry('task_falcon1_run_merge_consensus_jobs', '0.0.0', [FC_JSON, RDJ, FC_FOFN], [FC_FOFN], is_distributed=True, nproc=1)
+@registry('task_falcon1_run_merge_consensus_jobs', '0.0.0', [FT_JSON, RDJ, FT_FOFN], [FT_FOFN], is_distributed=True, nproc=1)
 def run_rtc(rtc):
   with cd(os.path.dirname(rtc.task.output_files[0])):
     return pbfalcon.run_merge_consensus_jobs(rtc.task.input_files, rtc.task.output_files, db_prefix='preads')
 
-@registry('task_falcon2_run_asm', '0.0.0', [FC_JSON, FC_FOFN], [FileTypes.FASTA], is_distributed=False)
+@registry('task_falcon2_run_asm', '0.0.0', [FT_JSON, FT_FOFN], [FT_FASTA], is_distributed=False)
 def run_rtc(rtc):
   with cd(os.path.dirname(rtc.task.output_files[0])):
     return pbfalcon.run_falcon_asm(rtc.task.input_files, rtc.task.output_files)
 
-@registry('task_falconx', '0.0.0', [FC_JSON, FC_FOFN], [FileTypes.FASTA], is_distributed=False)
+@registry('task_falconx', '0.0.0', [FT_JSON, FT_FOFN], [FT_FASTA], is_distributed=False)
 def run_rtc(rtc):
   with cd(os.path.dirname(rtc.task.output_files[0])):
     return pbfalcon.run_falconx(rtc.task.input_files, rtc.task.output_files)
