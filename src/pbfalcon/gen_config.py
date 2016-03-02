9f3abd00247de4a811394e27a51051ca6991f3ed
Christopher Dunn <cdunn@pacificbiosciences.com>
2016-03-02 08:59:14 -0800

    seed_coverage
    But for lambda , we might need length_cutoff/-x1200

--- src/pbfalcon/gen_config.py
+++ src/pbfalcon/gen_config.py
@@ -22,19 +22,20 @@ OPTION_CFG = 'HGAP_FalconAdvanced_str'
 
 defaults_old = """\
 falcon_sense_option = --output_multi --min_idt 0.70 --min_cov 1 --max_n_read 20000 --n_core 6
-length_cutoff = 1
+length_cutoff = 1000
 length_cutoff_pr = 1
 pa_DBsplit_option = -x5 -s50 -a
-pa_HPCdaligner_option =  -v -k25 -h35 -w5 -H1000 -e.95 -l40 -s1000 -t27
+pa_HPCdaligner_option =  -v -k25 -h35 -w5 -e.95 -l40 -s1000 -t27
 pa_concurrent_jobs = 32
 overlap_filtering_setting = --max_diff 10000 --max_cov 100000 --min_cov 0 --bestn 1000 --n_core 4
-ovlp_HPCdaligner_option =  -v -k25 -h35 -w5 -H1000 -e.99 -l40 -s1000 -t27
+ovlp_HPCdaligner_option =  -v -k25 -h35 -w5 -e.99 -l40 -s1000 -t27
 ovlp_DBsplit_option = -x5 -s50 -a
 ovlp_concurrent_jobs = 32
 """
 old_defaults_lambda = """\
+genome_size = 48000
 falcon_sense_option = --output_multi --min_idt 0.70 --min_cov 4 --max_n_read 200 --n_core 6
-length_cutoff = 12000
+length_cutoff = -1
 length_cutoff_pr = 12000
 pa_DBsplit_option = -x500 -s50
 pa_HPCdaligner_option = -v -dal4 -t16 -e.70 -l1000 -s1000
@@ -49,20 +50,24 @@ ovlp_concurrent_jobs = 32
 # draft assembly of 49820b. I meant to experiment some more, but we are in a rush to
 # to update defaults in 3.0.4 for ecoli.
 defaults_lambda = """
+genome_size = 48502
+seed_coverage = 40
 falcon_sense_option = --output_multi --min_idt 0.77 --min_cov 10 --max_n_read 2000 --n_core 6
-length_cutoff = 1200
+length_cutoff = -1
 length_cutoff_pr = 50
 overlap_filtering_setting = --max_diff 1000 --max_cov 100000 --min_cov 0 --bestn 1000 --n_core 4
 ovlp_DBsplit_option = -s50 -a
 ovlp_hpcdaligner_option = -v -k15 -h60 -w6 -e.95 -l40 -s100 -M16
 ovlp_concurrent_jobs = 32
-pa_DBsplit_option = -x1200 -s500 -a
-pa_HPCdaligner_option =   -v -k16 -h35 -w7 -e.70 -l40 -s100 -M16
+pa_DBsplit_option = -s500 -a
+pa_HPCdaligner_option =   -v -k15 -h35 -w7 -e.70 -l40 -s100 -M16
 pa_concurrent_jobs = 32
 """
 # ecoli based on Jim's run: http://smrtlink-beta:8080/#/analysis-job/2437
 defaults_ecoli = """
-length_cutoff = 9000
+genome_size = 4500000
+seed_coverage = 20.5
+length_cutoff = -1
 length_cutoff_pr = 500
 falcon_sense_option = --output_multi --min_idt 0.70 --min_cov 4 --max_n_read 200 --n_core 6
 overlap_filtering_setting = --max_diff 200 --max_cov 100 --min_cov 2 --bestn 10 --n_core 4
@@ -72,31 +77,34 @@ pa_DBsplit_option = -x500 -s200
 pa_HPCdaligner_option =   -v -dal24 -t14 -h35 -e.70 -l1000 -s100
 """
 defaults_yeast = """
+genome_size = 12000000
 cns_concurrent_jobs = 32
 falcon_sense_option = --output_multi --min_idt 0.70 --min_cov 4 --max_n_read 200 --n_core 8
-length_cutoff = 15000
+length_cutoff = -1
 length_cutoff_pr = 500
 overlap_filtering_setting = --max_diff 40 --max_cov 80 --min_cov 2 --n_core 12
 ovlp_DBsplit_option = -x15000 -s40
 ovlp_HPCdaligner_option =  -v -dal4 -k24 -e.96  -s1000         -t32 -l2500 -h1024
 ovlp_concurrent_jobs = 32
 pa_DBsplit_option = -a -x500 -s500
-pa_HPCdaligner_option =    -v -dal4 -k18 -e0.70 -s1000 -H15000 -t16 -l4800 -h480 -w8
+pa_HPCdaligner_option =    -v -dal4 -k18 -e0.70 -s1000 -t16 -l4800 -h480 -w8
 """
 defaults_human = """
+genome_size = 3000000000
 cns_concurrent_jobs = 32
 falcon_sense_option = --output_multi --min_idt 0.70 --min_cov 4 --max_n_read 200 --n_core 8
-length_cutoff = 15000
+length_cutoff = -1
 length_cutoff_pr = 500
 overlap_filtering_setting = --max_diff 40 --max_cov 80 --min_cov 2 --n_core 12
 ovlp_DBsplit_option = -x15000 -s40
 ovlp_HPCdaligner_option =  -v -dal4 -k24 -e.96  -s1000         -t32 -l2500 -h1024
 ovlp_concurrent_jobs = 32
 pa_DBsplit_option = -a -x500 -s500
-pa_HPCdaligner_option =    -v -dal4 -k18 -e0.70 -s1000 -H15000 -t16 -l4800 -h480 -w8
+pa_HPCdaligner_option =    -v -dal4 -k18 -e0.70 -s1000 -t16 -l4800 -h480 -w8
 """
 defaults_human_in_falcon = """
-length_cutoff = 10000
+genome_size = 3000000000
+length_cutoff = -1
 length_cutoff_pr = 7000
 pa_HPCdaligner_option =  -v -dal128 -t16 -e.70 -l1000 -s1000
 ovlp_HPCdaligner_option = -v -dal128 -t32 -h60 -e.96 -l500 -s1000
@@ -110,12 +118,13 @@ overlap_filtering_setting = --max_diff 60 --max_cov 60 --min_cov 2 --n_core 24
 """
 # See /lustre/hpcprod/jchin/CHM1_P6_project/asm3
 defaults_human_recent = """
-length_cutoff = 15000
+genome_size = 3000000000
+length_cutoff = -1
 length_cutoff_pr = 15000
 pa_DBsplit_option = -a -x500 -s400
-pa_HPCdaligner_option =  -v -dal128 -t16 -H15000 -e0.75 -M24 -l4800 -k18 -h480 -w8 -s100
+pa_HPCdaligner_option =  -v -dal128 -t16 -e0.75 -M24 -l4800 -k18 -h480 -w8 -s100
 ovlp_DBsplit_option = -s400
-ovlp_HPCdaligner_option =  -v -dal128  -M24 -k24 -h1024 -e.96 -l2500 -s100 -H15000
+ovlp_HPCdaligner_option =  -v -dal128  -M24 -k24 -h1024 -e.96 -l2500 -s100
 overlap_filtering_setting = --max_diff 40 --max_cov 80 --min_cov 2 --n_core 12
 falcon_sense_option = --output_multi --min_idt 0.70 --min_cov 4 --max_n_read 400 --n_core 12
 falcon_sense_skip_contained = False
@@ -215,7 +224,7 @@ def ini2option_text(ini):
 def get_falcon_overrides(cfg_content, OPTION_CFG=OPTION_CFG):
     """options keys are bare (no namespaces)
     """
-    if '\n' in cfg_content:
+    if '\n' in cfg_content and '' != cfg_content.strip():
         log.error('linefeed found in option "%s", which is ok here but should have been prevented earler' %(
             OPTION_CFG))
         cfg_content = ini2option_text(cfg_content)
@@ -242,13 +251,12 @@ def run_falcon_gen_config(input_files, output_files, options):
     o_cfg_fn, = output_files
     import pprint
     log.info('options to run_falcon_gen_config:\n{}'.format(pprint.pformat(options)))
-    print('options to run_falcon_gen_config:\n{}'.format(pprint.pformat(options)))
     options = _options_dict_with_base_keys(options)
     falcon_options = _populate_falcon_options(options)
-    print('falcon_options to run_falcon_gen_config:\n{}'.format(pprint.pformat(falcon_options)))
+    log.info('falcon_options to run_falcon_gen_config:\n{}'.format(pprint.pformat(falcon_options)))
     if OPTION_CFG in options:
         overrides = get_falcon_overrides(options[OPTION_CFG], OPTION_CFG)
-        print('overrides:\n%s'% pprint.pformat(overrides))
+        log.info('overrides:\n%s'% pprint.pformat(overrides))
         falcon_options.update(overrides)
     else:
         raise Exception("Could not find %s" %OPTION_CFG)
