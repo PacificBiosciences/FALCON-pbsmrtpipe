9e7e3901a8b2334f3f1445f74430f11828c94480
Christopher Dunn <cdunn@pacificbiosciences.com>
2016-09-16 10:11:36 -0700

    Moved Report/Attribute interactions to pbreports/report/preassembly.py

--- pbfalcon/report_preassembly.py
+++ pbfalcon/report_preassembly.py
@@ -22,7 +22,7 @@ Output of Original Report
 # Copied from
 #   http://swarm/files/depot/branches/springfield/S2.3/software/smrtanalysis/bioinformatics/tools/pbreports/pbreports/report/preassembly.py
 from __future__ import absolute_import
-from pbcommand.models.report import Report, Attribute
+from pbreports.report.preassembly import produce_report
 from falcon_polish.functional import stricter_json
 from falcon_kit.run_support import get_length_cutoff
 from falcon_kit import stats_preassembly
@@ -73,62 +73,6 @@ def for_task(
         log.info("Writing report to {!r}.".format(o_json_fn))
         content = report.to_json()
         ofs.write(content)
-
-def produce_report(
-        genome_length,
-        raw_reads,
-        raw_mean,
-        raw_n50,
-        raw_p95,
-        raw_bases,
-        raw_coverage,
-        length_cutoff,
-        seed_reads,
-        seed_bases,
-        seed_mean,
-        seed_n50,
-        seed_p95,
-        seed_coverage,
-        preassembled_reads,
-        preassembled_mean,
-        preassembled_n50,
-        preassembled_p95,
-        preassembled_bases,
-        preassembled_coverage,
-        preassembled_yield,
-        preassembled_seed_fragmentation,
-        preassembled_seed_truncation,
-        **ignored
-    ):
-    #preassembled_yield = '{:.3f}'.format(preassembled_yield) # but this would make it a str, unlike the others.
-    # Report Attributes
-    attrs = []
-    attrs.append(Attribute('genome_length', genome_length, name="Genome Length (user input)"))
-    attrs.append(Attribute('raw_reads', raw_reads, name="Number of Raw Reads"))
-    attrs.append(Attribute('raw_mean', raw_mean, name="Raw Read Length Mean"))
-    attrs.append(Attribute('raw_n50', raw_n50, name="Raw Read Length (N50)"))
-    attrs.append(Attribute('raw_p95', raw_p95, name="Raw Read Length 95%"))
-    attrs.append(Attribute('raw_bases', raw_bases, name="Number of Raw Bases (total)"))
-    attrs.append(Attribute('raw_coverage', raw_coverage, name="Raw Coverage (bases/genome_size)"))
-    attrs.append(Attribute('length_cutoff', length_cutoff, name="Length Cutoff (user input or auto-calc)"))
-    attrs.append(Attribute('seed_reads', seed_reads, name="Number of Seed Reads"))
-    attrs.append(Attribute('seed_mean', seed_mean, name="Seed Read Length Mean"))
-    attrs.append(Attribute('seed_n50', seed_n50, name="Seed Read Length (N50)"))
-    attrs.append(Attribute('seed_p95', seed_p95, name="Seed Read Length 95%"))
-    attrs.append(Attribute('seed_bases', seed_bases, name="Number of Seed Bases (total)"))
-    attrs.append(Attribute('seed_coverage', seed_coverage, name="Seed Coverage (bases/genome_size)"))
-    attrs.append(Attribute('preassembled_reads', preassembled_reads, name="Number of Pre-Assembled Reads"))
-    attrs.append(Attribute('preassembled_mean', preassembled_mean, name="Pre-Assembled Read Length Mean"))
-    attrs.append(Attribute('preassembled_n50', preassembled_n50, name="Pre-Assembled Read Length (N50)"))
-    attrs.append(Attribute('preassembled_p95', preassembled_p95, name="Pre-Assembled Read Length 95%"))
-    attrs.append(Attribute('preassembled_bases', preassembled_bases, name="Number of Pre-Assembled Bases (total)"))
-    attrs.append(Attribute('preassembled_coverage', preassembled_coverage, name="Pre-Assembled Coverage (bases/genome_size)"))
-    attrs.append(Attribute('preassembled_yield', preassembled_yield, name="Pre-Assembled Yield (bases/seed_bases)"))
-    attrs.append(Attribute('preassembled_seed_fragmentation', preassembled_seed_fragmentation, name="Avg number of reads that each seed is broken into."))
-    attrs.append(Attribute('preassembled_seed_truncation', preassembled_seed_truncation, name="Avg number of bases lost from each seed."))
-
-    report = Report('preassembly', title='Preassembly', attributes=attrs)
-    return report
 
 def write_report_from_stats(stats_ifs, report_ofs):
     """This is used by HGAP5, task_run_hgap.py.
