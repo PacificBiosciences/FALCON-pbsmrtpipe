c9da4ca5eb21ad7d3d4524ef32d7bb1678f75e43
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-09-22 10:52:08 -0700

    Must actually touch the output sentinel

--- pbfalcon/tasks/basic.py
+++ pbfalcon/tasks/basic.py
@@ -213,8 +213,7 @@ def run_rtc(rtc):
 # remove all las files regardless
 def run_rtc(rtc):
   with cd(os.path.dirname(rtc.task.output_files[0])):
-    return 0
-    #return pbfalcon.run_rm_las(rtc.task.input_files, rtc.task.output_files, prefix='')
+    return pbfalcon.run_rm_las(rtc.task.input_files, rtc.task.output_files, prefix='QWERTY')
 
 @registry('task_report_preassembly_yield', '0.0.0', [FT_JSON, FT_FOFN, FT_DB, FT_TXT], [FT(FT_REPORT, 'preassembly_yield', "Preassembly report")], is_distributed=False)
 def run_rtc(rtc):
