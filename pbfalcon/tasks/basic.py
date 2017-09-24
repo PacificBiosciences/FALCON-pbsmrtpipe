a64c4b8cbb61196dd5b2965fd8764ff4d46f81e7
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-09-23 22:55:15 -0700

    Keep preads.N.las
    Those are needed by fc_ovlp_filter_with_phase.py
    in falcon-unzip.

--- pbfalcon/tasks/basic.py
+++ pbfalcon/tasks/basic.py
@@ -214,7 +214,8 @@ def run_rtc(rtc):
 # remove all las files regardless
 def run_rtc(rtc):
   with cd(os.path.dirname(rtc.task.output_files[0])):
-    return pbfalcon.run_rm_las(rtc.task.input_files, rtc.task.output_files, prefix='preads*.')
+    #return pbfalcon.run_rm_las(rtc.task.input_files, rtc.task.output_files, prefix='preads*.')
+    return pbfalcon.run_rm_las(rtc.task.input_files, rtc.task.output_files, prefix='preads.*.preads.')
 
 @registry('task_report_preassembly_yield', '0.0.0', [FT_JSON, FT_FOFN, FT_DB, FT_TXT], [FT(FT_REPORT, 'preassembly_yield', "Preassembly report")], is_distributed=False)
 def run_rtc(rtc):
