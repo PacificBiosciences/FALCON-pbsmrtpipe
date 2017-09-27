4d16c480df85a2c5b1faf7b40074d8623c4ace96
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-09-27 08:39:31 -0700

    setup and build

--- bamboo_build.sh
+++ bamboo_build.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
+source bamboo_setup.sh
+
 set -vex
 echo 'hi'
+
+pip install --user nose
+pip install --user pytest
+
+pushd ../pbcommand
+pip install --user --edit .
+popd
+
+pushd ../pbreports
+pip install --user --edit .
+popd
+
+pushd ../pypeFLOW
+pip install --user --edit .
+popd
+
+pushd ../FALCON
+pip install --user --edit .
+popd
+
+pushd ../FALCON-polish
+pip install --user --edit .
+popd
+
+pip install --user --edit .
+
+make utest
