841f669016ae296926469deb1e5e4d71989e95c9
Christopher Dunn <cdunn@pacificbiosciences.com>
2018-03-15 14:22:57 -0700

    bugfix: Substitute rel-paths with absolute when gathering
    Not fool-proof, but this works by convention, where for each record
    the value might be a filepath relative to the directory of that
    results file.
    
    We *could* re-relativize, but since this is really a hack, it
    seems better to be as transparent as possible.

--- pbfalcon/pbtag/mains/gather_json_list.py
+++ pbfalcon/pbtag/mains/gather_json_list.py
@@ -8,17 +8,30 @@ from __future__ import unicode_literals
 from ..pbcommand import load_pipeline_chunks_from_json
 import json
 import logging
+import os
+import re
 
 LOG = logging.getLogger(__name__)
 
 
 def gather(input_files, output_file):
     """Combine lists of data into a single list of data.
+    At first level of each record, substitute './' with '{filedir}/'
+    in value. (We assume these are filenames relative to the directory
+    of the input_file.)
     """
     data = []
+    re_reldir = re.compile(r'^\./') # Find literal './'
     for input_file in input_files:
         with open(input_file) as stream:
             these_data = json.loads(stream.read())
+            filedir = os.path.abspath(os.path.dirname(input_file)) + '/'
+            for rec in these_data:
+                for k,v in list(rec.items()):
+                    mo = re_reldir.search(v)
+                    if not mo:
+                        continue
+                    rec[k] = re_reldir.sub(filedir, v) # over-write w/ abspath
             data.extend(these_data)
     with open(output_file, "w") as stream:
         stream.write(json.dumps(data, indent=None, separators=(',', ':')))
